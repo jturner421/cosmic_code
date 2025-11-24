14fbd03e27b883a563e3ce886092db6ad9707843
MCP Server Team <mcp@example.com>
2025-11-24 08:36:40 -0600

    feat: add service layer and reorganize tests
    Create initial service layer with add_batch and allocate functions to provide orchestration between domain model and repository. Reorganize test suite into unit and integration folders for better separation of concerns.

---
+++ service_layer/services.py
@@ -0,0 +1,30 @@
+from datetime import date
+
+from domain.model import OrderLine, allocate, Batch
+from repository import AbstractRepository
+from repository.repositories import SqlAlchemyRepository
+
+
+class InvalidSku(Exception):  # noqa: N818
+    pass
+
+
+def is_valid_sku(sku: str, batches) -> bool:
+    return sku in {b.sku for b in batches}
+
+def add_batch(
+    ref: str, sku: str, qty: int, eta: date | None, repo: AbstractRepository | SqlAlchemyRepository, session,
+) -> None:
+    repo.add(Batch(ref, sku, qty, eta))
+    session.commit()
+
+def allocate(
+    line: OrderLine, repo: AbstractRepository | SqlAlchemyRepository, session
+) -> str:
+    batches = repo.list()
+    if not is_valid_sku(line.sku, batches):
+        error = f" Invalid sku {line.sku}"
+        raise InvalidSku(error)
+    batchref = allocate(line, batches)
+    session.commit()
+    return batchref
